aa2f758c26f2413240cbd09d4b79929db5c5e973
devbeast0121 <devbeast3800@gmail.com>
2025-10-23 01:11:15 +0900

    update: muxvideoplayer

--- mux-adalo-components/components/MuxVideoPlayer/index.js
+++ mux-adalo-components/components/MuxVideoPlayer/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const MuxVideoPlayer = ({
     playbackId,
